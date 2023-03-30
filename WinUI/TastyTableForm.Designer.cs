2ab5dbf79731fda57d5e381d44ce9c7c5b829bc9
GothicLolita229 <ceciliathomas4216@gmail.com>
2023-03-30 15:09:39 -0400

    Initial Commit

--- WinUI/TastyTableForm.Designer.cs
+++ WinUI/TastyTableForm.Designer.cs
@@ -28,20 +28,35 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.EnterRecipeLabel = new System.Windows.Forms.Label();
 			this.SuspendLayout();
+			// 
+			// EnterRecipeLabel
+			// 
+			this.EnterRecipeLabel.AutoSize = true;
+			this.EnterRecipeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.EnterRecipeLabel.Location = new System.Drawing.Point(41, 40);
+			this.EnterRecipeLabel.Name = "EnterRecipeLabel";
+			this.EnterRecipeLabel.Size = new System.Drawing.Size(165, 28);
+			this.EnterRecipeLabel.TabIndex = 0;
+			this.EnterRecipeLabel.Text = "Enter Recipe:";
 			// 
 			// TastyTableForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.EnterRecipeLabel);
 			this.Name = "TastyTableForm";
 			this.Text = "Tasty Table Lite";
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Label EnterRecipeLabel;
 	}
 }
 
--- WinUI/TastyTableForm.cs
+++ WinUI/TastyTableForm.cs
@@ -16,5 +16,7 @@ namespace WinUI
 		{
 			InitializeComponent();
 		}
+
+
 	}
 }
