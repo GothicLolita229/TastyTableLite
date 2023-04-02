372bf3f67b8c65dfdfd8d4a46d04ea12d81014a3
GothicLolita229 <ceciliathomas4216@gmail.com>
2023-04-02 17:50:46 -0400

    Did a little formatting of the page
    Added logo and Title and footnote float thing.

--- WinUI/TastyTableForm.Designer.cs
+++ WinUI/TastyTableForm.Designer.cs
@@ -28,35 +28,80 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.EnterRecipeLabel = new System.Windows.Forms.Label();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TastyTableForm));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.panel2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
 			this.SuspendLayout();
 			// 
-			// EnterRecipeLabel
+			// panel1
 			// 
-			this.EnterRecipeLabel.AutoSize = true;
-			this.EnterRecipeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.EnterRecipeLabel.Location = new System.Drawing.Point(41, 40);
-			this.EnterRecipeLabel.Name = "EnterRecipeLabel";
-			this.EnterRecipeLabel.Size = new System.Drawing.Size(165, 28);
-			this.EnterRecipeLabel.TabIndex = 0;
-			this.EnterRecipeLabel.Text = "Enter Recipe:";
+			this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(133)))), ((int)(((byte)(45)))));
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 841);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1736, 92);
+			this.panel1.TabIndex = 1;
+			// 
+			// panel2
+			// 
+			this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(230)))), ((int)(((byte)(204)))));
+			this.panel2.Controls.Add(this.label1);
+			this.panel2.Controls.Add(this.pictureBox1);
+			this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+			this.panel2.Location = new System.Drawing.Point(0, 0);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1736, 92);
+			this.panel2.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(12, -1);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(95, 93);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe Print", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(765, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(243, 65);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Tasty Table";
 			// 
 			// TastyTableForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.EnterRecipeLabel);
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(230)))), ((int)(((byte)(204)))));
+			this.ClientSize = new System.Drawing.Size(1736, 933);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.Name = "TastyTableForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Tasty Table Lite";
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
 			this.ResumeLayout(false);
-			this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.Label EnterRecipeLabel;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.PictureBox pictureBox1;
 	}
 }
 
